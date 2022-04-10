fda80d55017b6c6e047dbd0e076d654e9bbb3ef1
Maria Katrina Ocampo <ocampo.mkb@gmail.com>
2022-04-10 14:38:03 -0300

    size

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include "includes/header.php";
 <section id="Welcome" style="box-sizing:border-box; width: 100%; height: 100vh;background-color:#8e7f97";>
     <div style="padding:100px;text-align:center">    
         <div style="position:relative;margin:auto;">
-            <img src="assets/kat.gif" style="border-radius: 50%; max-width:70%; max-height:70%"/>
+            <img src="assets/kat.gif" style="border-radius: 50%; max-width:50%; max-height:50%"/>
         </div>
     </div>
 </section>
