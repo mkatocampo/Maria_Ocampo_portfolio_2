0656e024306faa21ad4088834b845e4ea05de0a4
Maria Katrina Ocampo <ocampo.mkb@gmail.com>
2022-04-10 16:00:50 -0300

    fixed footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
-<footer style="text-align:center;background:linear-gradient(pink,lightblue); opacity:0.6">
-<hr>
+<footer style="text-align:center;background:linear-gradient(pink,lightblue); opacity:0.6;padding:15px;">
       &copy; 2022 Maria Ocampo
-    </footer>
+</footer>
 </body>
 
 </html>
